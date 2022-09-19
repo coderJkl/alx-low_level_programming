00854ed410b300b4f2c1faf5539c9d6c97bd3daf
Abduljelil Obaro <obaroabduljeil@gmail.com>
2022-09-19 13:01:46 -0700

    project 1

---
+++ 0x02-funtions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * print_alphabet - prints lower case alphabet from a to z
+ *
+ * @i: while loop iterator
+ */
+void print_alphabet(void)
+{
+	int i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		_putchar(i);
+	
+	}
+		_putchar('\n');
+}
+
