0225447d8fda742a717c3ae79cb83715b21f624f
Abduljelil Obaro <obaroabduljeil@gmail.com>
2022-09-20 13:19:13 -0700

    a new project

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev -  prints a reversed string and a new line, to stdout
+ * _strlen - counts the characters in a string
+ * @s: pointer to string
+ * Return: reverse strings plus new-line
+ */
+void print_rev(char *s)
+{
+	int i, k;
+
+	i = _strlen(s) - 1;
+	while (i >= 0)
+	{
+		k = s[i];
+		_putchar(k);
+		--i;
+	}
+	_putchar('\n');
+}
