357d71e16737c138f4e997e12ca320740b11ff3a
coderJkl <obaroabduljelil@gmail.com>
2022-12-08 01:23:17 -0800

    updates

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  * main - Entry point
  *
@@ -7,10 +6,10 @@
  */
 int main()
 {
-    printf("Size of char: %2d byte(s)\n",sizeof(char));
-    printf("Size of an int: %2d byte(s)\n",sizeof(int);
-    printf("Size of a long int: %2d byte(s)\n",sizeof(long int));
-    printf("Size of an float: %2d byte(s)\n",sizeof(float));
+    printf("Size of a char: %lu byte(s)\n",sizeof(char));
+    printf("Size of an int: %lu byte(s)\n",sizeof(int);
+    printf("Size of a long long int: %lu byte(s)\n",sizeof(long int));
+    printf("Size of a float: %lu byte(s)\n",sizeof(float));
 
 	return (0);
 }
