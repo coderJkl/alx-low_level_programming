d58f81963a0fb920dcb8c353380f14a5d9f5738a
coderJkl <obaroabduljelil@gmail.com>
2022-12-10 03:06:28 -0800

    printing base 10 numbrs using putchar

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -4,34 +4,14 @@
  *
  * Return: Always 0 ( on success)
  */
-void my_putchar(int var);
-
 int main(void)
 {
 	int a;
 
-	for (a = 0; a <= 10; a++)
+	for (a = 0; a < 10; a++)
 	{
-		my_putchar(a);
+		putchar(a % 11 + '0');
 	}
 	putchar('\n');
 	return (0);
-}
-
-void my_putchar(int var) {
-
-    /* print '-' for negative numbers */
-    if (var < 0) {
-        putchar('-');
-        var = var * -1;
-    }
-
-    /* First remove the last digit of number and print
-    the remaining digits using recursion, then print
-    the last digit
- */
-    if (var/10)
-        my_putchar(var/10);
-
-    putchar(var%10 + 0);
 }
