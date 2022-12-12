d71734a61f3d6dfebe9accd45685776ff4251d52
coderJkl <obaroabduljelil@gmail.com>
2022-12-12 08:35:31 -0800

    completing the mandatory projects

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero
+*/
+
+int main(void)
+{
+	int x, count;
+	count = 0;
+
+	for (x = 0; x < 10; x++)
+		putchar(x + '0');
+	for (x = 0; x < 6; x++)
+	{
+		putchar('a' + count);
+		count++;
+	}
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always zero
+*/
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		putchar(i + '0');
+		if (i == 9)
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	return (0);
+}
