5478b37b1af9c16224b1fc28d1fa71557386a4db
coderJkl <obaroabduljelil@gmail.com>
2022-12-15 08:08:34 -0800

    modified

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -10,10 +10,11 @@ void print_line(int n)
 {
 	int i = 0;
 
-	while (i <= n)
-	{
-		_putchar('_');
-		i++;
-	}
+	if (n > 0)
+		while (i <= n)
+		{
+			_putchar('_');
+			i++;
+		}
 	_putchar('\n');
 }
