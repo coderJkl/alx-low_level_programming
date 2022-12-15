42b2f92b71329dd53c87446dc7044f5204a4a27d
coderJkl <obaroabduljelil@gmail.com>
2022-12-14 17:31:40 -0800

    update

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -7,19 +7,21 @@
  */
 int main(void)
 {
-	int i, n , tmp1, tmp2;
+	long int n;
+	int i, tmp1, tmp2;
 
 	i = 0;
 	n = 1;
-	while (i < 50)
+	while (i < 25)
 	{
-		printf("%d\n", n);
+		printf("%ld, ", n);
 		tmp1 = n;
 		n += tmp1;
-		printf("%d\n", n);
+		printf("%ld, ", n);
 		tmp2 = n;
-		n = tmp1 + tmp2;	
+		n = tmp1 + tmp2;
 		i++;
 	}
+	putchar('\n');
 	return (0);
 }
