2b5a1dc085ffa70b1ce8024db5bc475bd01e8c1e
coderJkl <obaroabduljelil@gmail.com>
2022-12-17 09:09:48 -0800

    project #9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 1 to 100
+ * subtituting "fizz" and "buzz" for multilples of 3 and 5
+ * and "fizzbuzz" for multiples of both 3 and 5
+ *
+ * Return: 0 Always (success)
+ */
+
+int main(void)
+{
+	int i = 1;
+
+	while (i > 0 && i <= 100)
+	{
+		if (i % 3 != 0 && i % 5 != 0)
+		{
+			printf("%d ", i);
+		}
+		else
+		{
+			if (i % 3 == 0)
+			{
+				printf("Fizz");
+			}
+			if (i % 5 == 0)
+			{
+				printf("Buzz");
+			}
+			putchar(' ');
+		}
+		++i;
+	}
+	return (0);
+}
