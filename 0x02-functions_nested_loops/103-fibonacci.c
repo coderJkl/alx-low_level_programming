52ac5cb60aba2547dc1ba19f2ee78615500cab35
coderJkl <obaroabduljelil@gmail.com>
2022-12-18 02:24:51 -0800

    #advance project #103

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - program prints sum of the first fibonacci even numbers less than 4000000
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	long int sum = 2;
+
+	long int f1 = 1, f2 = 2;
+
+	long int f = f1 + f2;
+
+	while (f <= 4000000)
+	{
+		if (f % 2 == 0)
+			sum += f;
+		f1 = f2;
+		f2 = f;
+		f = f1 + f2;
+	}
+	printf("%ld\n", sum);
+	return (0);
+}
