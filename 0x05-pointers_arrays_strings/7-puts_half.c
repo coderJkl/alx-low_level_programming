43808c71bd8582adb6b0c5ac8689277906ae5d00
coderJkl <obaroabduljelil@gmail.com>
2022-12-20 04:48:35 -0800

    new project

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,27 +1,23 @@
 #include "main.h"
 #include <string.h>
+
 /**
- * puts_half -  prints half the characters in a string.
  * _strlen - counts the characters in a string
  * @str: pointer to string
  * Return: reverse strings plus new-line
  */
+
 void puts_half(char *str)
 {
-	int i, m, j;
+	int i = strlen(str);
 
-	m = strlen(str);
-	if (m % 2 == 0)
+	if (i % 2 != 0)
+		i = i - 2;
+	i = i / 2;
+	while (str[i])
 	{
-	i = m / 2;
-	for (; i < m; ++i)
-	{
-		j = str[i];
-		_putchar(j);
+		_putchar(str[i]);
+		i++;
 	}
 	_putchar('\n');
-	}
-	else
-		--m;
-
 }
