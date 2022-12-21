1516a5426665a9f7a8a92bbd3aa3fd502ebd5f96
coderJkl <obaroabduljelil@gmail.com>
2022-12-21 06:52:10 -0800

    new projects

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,16 +1,23 @@
 #include "main.h"
 #include "string.h"
+
 /**
  * _strcat - appends a *src string to another *dest
  * @src : source string
  * @dest: destination string
  * Return: A character
  */
+
 char *_strcat(char *dest, char *src)
 {
-	char *save = dest;
+	int i = strlen(dest), j = strlen(src), k = 0;
 
-	for (; *dest; ++dest);
-	while ((*dest++ = *src++) != '\0');
-	return (save);
+	while (k < j)
+	{
+		dest[i] = src[k];
+		k++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
 }
--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,20 +1,24 @@
 #include "main.h"
 #include "string.h"
+
 /**
- * _strncat - appends a *src string to another *dest
- *_strlen - counts the characters in a string
+ * _strncat - appends n bytes of string  from a *src string to another *dest
  * @src : source string
  * @dest: destination string
+ * @n: number of bytes
  * Return: A character
  */
+
 char *_strncat(char *dest, char *src, int n)
 {
-	int dest_len = strlen(dest);
-	int i;
+	int i = strlen(dest), k = 0;
 
-	for (i = 0; i < n && src[i] != '\0'; i++)
-		dest[dest_len + i] = src[i];
-	dest[dest_len + i] = '\0';
-
+	while (k < n)
+	{
+		dest[i] = src[k];
+		k++;
+		i++;
+	}
+	dest[i] = '\0';
 	return (dest);
 }
