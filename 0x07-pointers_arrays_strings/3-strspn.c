de59ec3fcd9932c111bd36d9d403e0b6e4bde2a5
coderJkl <obaroabduljelil@gmail.com>
2023-01-02 07:07:28 -0800

    project #3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,4 +1,28 @@
 #include "main.h"
+#include <string.h>
 
 /**
- * _strspn - 
+ * _strspn - Counts the bytes in  @s consisting only of bytes from @accept
+ * @s: Parent string
+ * @accept: string to compare
+ * Return: integer
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	char *p = s;
+	char *i;
+
+	while (*s)
+	{
+		for (i = accept; *i; i++)
+		{
+			if (*s == *i)
+				break;
+		}
+		if (*i == '\0')
+			break;
+		s++;
+	}
+	return ((unsigned int)(s - p));
+}
