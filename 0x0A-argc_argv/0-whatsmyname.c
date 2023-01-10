df3d32df433f09c5d8703fc7023f5ceb07766507
coderJkl <obaroabduljelil@gmail.com>
2023-01-10 10:43:42 -0800

    updates

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,9 +4,11 @@
  * main - To test the program
  * @argc: the number of command line argument
  * @argv: An array containing the program commland line arguments
+ * Return: 0
  */
 
-int main (__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+int main(__attribute__((unused)) int argc,
+		__attribute__((unused)) char *argv[])
 {
 	printf("%s\n", argv[0]);
 	return (0);
