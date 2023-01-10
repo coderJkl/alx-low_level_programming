967da7de41e4df4140c62909414e93c895245864
coderJkl <obaroabduljelil@gmail.com>
2023-01-10 10:40:59 -0800

    new project on main arguments

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main - To test the program
+ * @argc: the number of command line argument
+ * @argv: An array containing the program commland line arguments
+ */
+
+int main (__attribute__((unused)) int argc, __attribute__((unused)) char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
