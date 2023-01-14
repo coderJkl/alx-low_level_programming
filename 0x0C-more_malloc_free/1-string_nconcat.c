5af0da6f8ccde10c36fdf4dbae6f6805964804c6
coderJkl <obaroabduljelil@gmail.com>
2023-01-14 04:10:25 -0800

    changes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,6 +18,16 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return (NULL);
 	for (i = 0; i < strlen(s1); i++)
 		ptr[i] = s1[i];
+	if (n >= strlen(s2))
+	{
+		for (j = 0; j < strlen(s2); j++)
+		{
+			ptr[i] = s2[j];
+			i++;
+		}
+		ptr[i] = '\0';
+		return (ptr);
+	}
 	for (j = 0; j < n; j++)
 	{
 		ptr[i] = s2[j];
