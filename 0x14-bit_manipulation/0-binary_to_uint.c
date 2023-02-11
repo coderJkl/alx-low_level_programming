d3ec36d7d7fb4dafe3a33f56f7ef9c4011fa6f05
coderJkl <obaroabduljelil@gmail.com>
2023-02-11 10:41:23 -0800

    new project

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * binary_to_unit - converts binary to decimal
+ * @b - pointer to string
+ * Return: 0 or int convert
+ */
+
+unsigned int binary_to_uint(const char *b)
+{
+	unsigned int num = 0, mul = 1;
+	unsigned int len = strlen(b);
+
+	if (b == NULL)
+		return (0);
+
+	while (len--)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+
+		num += (b[len] - '0') * mul;
+		mul *= 2;
+	}
+
+	return (num);	
+}
