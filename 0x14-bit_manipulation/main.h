a8bbce07887e996c8d9d443c4120bc102bdffbdd
coderJkl <obaroabduljelil@gmail.com>
2023-02-13 22:03:22 -0800

    updates

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,6 +5,9 @@
 
 unsigned int binary_to_uint(const char *b);
 int _strlen(const char *s);
-
+int _putchar(char c);
+void print_binary(unsigned long int n);
+void c_bin(unsigned long int n);
+int get_bit(unsigned long int n, unsigned int index);
 
 #endif 
